9697f62cc83577602030a200ff21bc0ec212d333
Aldair-Desarrollador-de-Software <githubrepositorio5@gmail.com>
2025-05-27 17:39:43 -0600

    Add files via upload

---
+++ Sistema de ventas/Sistema de ventas/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Sistema_de_ventas
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnAgregarProducto;
+        private System.Windows.Forms.ComboBox cmbProductos;
+        private System.Windows.Forms.NumericUpDown numCantidad;
+        private System.Windows.Forms.Button btnAgregarCarrito;
+        private System.Windows.Forms.ListBox lstCarrito;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnEliminarProducto;
+        private System.Windows.Forms.Label lblDinero;
+        private System.Windows.Forms.TextBox txtDinero;
+        private System.Windows.Forms.Button btnCobrar;
+        private System.Windows.Forms.Label lblCambio;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnAgregarProducto = new System.Windows.Forms.Button();
+            this.cmbProductos = new System.Windows.Forms.ComboBox();
+            this.numCantidad = new System.Windows.Forms.NumericUpDown();
+            this.btnAgregarCarrito = new System.Windows.Forms.Button();
+            this.lstCarrito = new System.Windows.Forms.ListBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnEliminarProducto = new System.Windows.Forms.Button();
+            this.lblDinero = new System.Windows.Forms.Label();
+            this.txtDinero = new System.Windows.Forms.TextBox();
+            this.btnCobrar = new System.Windows.Forms.Button();
+            this.lblCambio = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.Location = new System.Drawing.Point(20, 20);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(100, 23);
+            this.lblProducto.TabIndex = 0;
+            this.lblProducto.Text = "Producto:";
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Location = new System.Drawing.Point(126, 17);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(174, 23);
+            this.txtProducto.TabIndex = 1;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.Location = new System.Drawing.Point(20, 55);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(100, 23);
+            this.lblPrecio.TabIndex = 2;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(126, 52);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(174, 23);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // btnAgregarProducto
+            // 
+            this.btnAgregarProducto.BackColor = System.Drawing.Color.LightGreen;
+            this.btnAgregarProducto.Location = new System.Drawing.Point(100, 85);
+            this.btnAgregarProducto.Name = "btnAgregarProducto";
+            this.btnAgregarProducto.Size = new System.Drawing.Size(200, 30);
+            this.btnAgregarProducto.TabIndex = 4;
+            this.btnAgregarProducto.Text = "Agregar/Actualizar";
+            this.btnAgregarProducto.UseVisualStyleBackColor = false;
+            this.btnAgregarProducto.Click += new System.EventHandler(this.btnAgregarProducto_Click);
+            // 
+            // cmbProductos
+            // 
+            this.cmbProductos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProductos.Location = new System.Drawing.Point(20, 130);
+            this.cmbProductos.Name = "cmbProductos";
+            this.cmbProductos.Size = new System.Drawing.Size(200, 23);
+            this.cmbProductos.TabIndex = 5;
+            // 
+            // numCantidad
+            // 
+            this.numCantidad.Location = new System.Drawing.Point(230, 130);
+            this.numCantidad.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numCantidad.Name = "numCantidad";
+            this.numCantidad.Size = new System.Drawing.Size(120, 23);
+            this.numCantidad.TabIndex = 6;
+            this.numCantidad.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnAgregarCarrito
+            // 
+            this.btnAgregarCarrito.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnAgregarCarrito.Location = new System.Drawing.Point(100, 165);
+            this.btnAgregarCarrito.Name = "btnAgregarCarrito";
+            this.btnAgregarCarrito.Size = new System.Drawing.Size(200, 30);
+            this.btnAgregarCarrito.TabIndex = 7;
+            this.btnAgregarCarrito.Text = "Agregar al carrito";
+            this.btnAgregarCarrito.UseVisualStyleBackColor = false;
+            this.btnAgregarCarrito.Click += new System.EventHandler(this.btnAgregarCarrito_Click);
+            // 
+            // lstCarrito
+            // 
+            this.lstCarrito.ItemHeight = 15;
+            this.lstCarrito.Location = new System.Drawing.Point(20, 210);
+            this.lstCarrito.Name = "lstCarrito";
+            this.lstCarrito.Size = new System.Drawing.Size(340, 94);
+            this.lstCarrito.TabIndex = 8;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblTotal.Location = new System.Drawing.Point(20, 360);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(200, 20);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total: $0.00";
+            // 
+            // btnEliminarProducto
+            // 
+            this.btnEliminarProducto.BackColor = System.Drawing.Color.LightCoral;
+            this.btnEliminarProducto.Location = new System.Drawing.Point(100, 320);
+            this.btnEliminarProducto.Name = "btnEliminarProducto";
+            this.btnEliminarProducto.Size = new System.Drawing.Size(200, 30);
+            this.btnEliminarProducto.TabIndex = 9;
+            this.btnEliminarProducto.Text = "Eliminar del carrito";
+            this.btnEliminarProducto.UseVisualStyleBackColor = false;
+            this.btnEliminarProducto.Click += new System.EventHandler(this.btnEliminarProducto_Click);
+            // 
+            // lblDinero
+            // 
+            this.lblDinero.Location = new System.Drawing.Point(20, 390);
+            this.lblDinero.Name = "lblDinero";
+            this.lblDinero.Size = new System.Drawing.Size(100, 23);
+            this.lblDinero.TabIndex = 11;
+            this.lblDinero.Text = "Dinero recibido:";
+            // 
+            // txtDinero
+            // 
+            this.txtDinero.Location = new System.Drawing.Point(130, 387);
+            this.txtDinero.Name = "txtDinero";
+            this.txtDinero.Size = new System.Drawing.Size(100, 23);
+            this.txtDinero.TabIndex = 12;
+            // 
+            // btnCobrar
+            // 
+            this.btnCobrar.BackColor = System.Drawing.Color.Orange;
+            this.btnCobrar.Location = new System.Drawing.Point(250, 385);
+            this.btnCobrar.Name = "btnCobrar";
+            this.btnCobrar.Size = new System.Drawing.Size(110, 30);
+            this.btnCobrar.TabIndex = 13;
+            this.btnCobrar.Text = "Cobrar";
+            this.btnCobrar.UseVisualStyleBackColor = false;
+            this.btnCobrar.Click += new System.EventHandler(this.btnCobrar_Click);
+            // 
+            // lblCambio
+            // 
+            this.lblCambio.Location = new System.Drawing.Point(20, 430);
+            this.lblCambio.Name = "lblCambio";
+            this.lblCambio.Size = new System.Drawing.Size(200, 20);
+            this.lblCambio.TabIndex = 14;
+            this.lblCambio.Text = "Cambio: $0.00";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(400, 520);
+            this.Controls.Add(this.lblProducto);
+            this.Controls.Add(this.txtProducto);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.btnAgregarProducto);
+            this.Controls.Add(this.cmbProductos);
+            this.Controls.Add(this.numCantidad);
+            this.Controls.Add(this.btnAgregarCarrito);
+            this.Controls.Add(this.lstCarrito);
+            this.Controls.Add(this.btnEliminarProducto);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblDinero);
+            this.Controls.Add(this.txtDinero);
+            this.Controls.Add(this.btnCobrar);
+            this.Controls.Add(this.lblCambio);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sistema de Ventas Minimalista";
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Sistema de ventas/Sistema de ventas/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Sistema_de_ventas
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, decimal> productos = new Dictionary<string, decimal>();
+        decimal total = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregarProducto_Click(object sender, EventArgs e)
+        {
+            string nombre = txtProducto.Text.Trim();
+            decimal precio;
+
+            if (nombre == "" || !decimal.TryParse(txtPrecio.Text, out precio))
+            {
+                MessageBox.Show("Ingrese producto y precio válidos.");
+                return;
+            }
+
+            productos[nombre] = precio;
+
+            cmbProductos.Items.Clear();
+            foreach (var p in productos.Keys)
+                cmbProductos.Items.Add(p);
+
+            txtProducto.Clear();
+            txtPrecio.Clear();
+        }
+
+        private void btnAgregarCarrito_Click(object sender, EventArgs e)
+        {
+            if (cmbProductos.SelectedItem == null)
+            {
+                MessageBox.Show("Seleccione un producto.");
+                return;
+            }
+
+            string nombre = cmbProductos.SelectedItem.ToString();
+            int cantidad = (int)numCantidad.Value;
+            decimal precio = productos[nombre];
+            decimal subtotal = precio * cantidad;
+
+            lstCarrito.Items.Add(nombre + " x" + cantidad + " = $" + subtotal.ToString("0.00"));
+            total += subtotal;
+            lblTotal.Text = "Total: $" + total.ToString("0.00");
+        }
+
+        private void btnEliminarProducto_Click(object sender, EventArgs e)
+        {
+            if (lstCarrito.SelectedIndex != -1)
+            {
+                string linea = lstCarrito.SelectedItem.ToString();
+                int idx1 = linea.LastIndexOf('$');
+                decimal valor = decimal.Parse(linea.Substring(idx1 + 1));
+                total -= valor;
+                lblTotal.Text = "Total: $" + total.ToString("0.00");
+
+                lstCarrito.Items.RemoveAt(lstCarrito.SelectedIndex);
+            }
+        }
+
+        private void btnCobrar_Click(object sender, EventArgs e)
+        {
+            decimal dinero;
+
+            if (!decimal.TryParse(txtDinero.Text, out dinero))
+            {
+                MessageBox.Show("Ingrese una cantidad válida.");
+                return;
+            }
+
+            if (dinero < total)
+            {
+                MessageBox.Show("Dinero insuficiente.");
+                return;
+            }
+
+            decimal cambio = dinero - total;
+            lblCambio.Text = "Cambio: $" + cambio.ToString("0.00");
+            MessageBox.Show("Gracias por su compra.", "Venta finalizada");
+
+            // Reiniciar
+            lstCarrito.Items.Clear();
+            total = 0;
+            lblTotal.Text = "Total: $0.00";
+            txtDinero.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+    }
+}
